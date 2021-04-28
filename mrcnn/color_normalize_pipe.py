b77316a52b0ac869cbaee0958f9a5d160ae21728
mahdi <mbehroozikhah@gmail.com>
2021-04-27 20:52:10 -0500

    fixed jpg files

--- mrcnn/color_normalize_pipe.py
+++ mrcnn/color_normalize_pipe.py
@@ -18,16 +18,11 @@ def reinhard_img(directory, des_path):
 
     for filename in os.listdir(directory):
         print(filename)
-        if filename.endswith(".png"):
-            img_input = cv2.imread(directory + filename)
-            plt.imshow(img_input), plt.show()
-            im_nmzd = htk.preprocessing.color_normalization.reinhard(img_input, mean_ref, std_ref)
-            status = cv2.imwrite(des_path + filename, im_nmzd)
-            plt.imshow(im_nmzd), plt.show()
-            print("Image written to file-system : ", status)
-            continue
-        else:
-            continue
-
+        img_input = cv2.imread(directory + filename)
+        plt.imshow(img_input), plt.show()
+        im_nmzd = htk.preprocessing.color_normalization.reinhard(img_input, mean_ref, std_ref)
+        status = cv2.imwrite(des_path + filename, im_nmzd)
+        plt.imshow(im_nmzd), plt.show()
+        print("Image written to file-system : ", status)
 
 reinhard_img(args.src, args.dest)
